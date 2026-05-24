13713d0aa6419c05dd46a76ddd2f805f5c88b5f5
Alex <Alex.J.Crosby19@gmail.com>
2026-05-24 14:13:02 +0100

    Added Player Death

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -3,17 +3,27 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] private int maxHealth = 10;
+    [SerializeField] private Animator characterAnimator;
 
     public int MaxHealth => maxHealth;
     public int CurrentHealth { get; private set; }
 
+    private bool isDead;
+
     private void Awake()
     {
         CurrentHealth = maxHealth;
+
+        if (characterAnimator == null)
+        {
+            characterAnimator = GetComponentInChildren<Animator>();
+        }
     }
 
     public void TakeDamage(int amount)
     {
+        if (isDead) return;
+
         CurrentHealth -= amount;
         CurrentHealth = Mathf.Max(CurrentHealth, 0);
 
@@ -21,7 +31,49 @@ public class PlayerHealth : MonoBehaviour
 
         if (CurrentHealth <= 0)
         {
-            Debug.Log("Player died / game over.");
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        isDead = true;
+
+        Debug.Log("Player died.");
+
+        if (characterAnimator != null)
+        {
+            characterAnimator.SetBool("Dead", true);
+        }
+
+        PlayerMovement movement = GetComponent<PlayerMovement>();
+        if (movement != null)
+        {
+            movement.enabled = false;
+        }
+
+        PlayerSpellCaster spellCaster = GetComponent<PlayerSpellCaster>();
+        if (spellCaster != null)
+        {
+            spellCaster.enabled = false;
+        }
+
+        EyeTargetingFireballCaster eyeFireball = GetComponent<EyeTargetingFireballCaster>();
+        if (eyeFireball != null)
+        {
+            eyeFireball.enabled = false;
+        }
+
+        TelekinesisSpellCaster telekinesis = GetComponent<TelekinesisSpellCaster>();
+        if (telekinesis != null)
+        {
+            telekinesis.enabled = false;
+        }
+
+        AOEKnockbackSpellCaster knockback = GetComponent<AOEKnockbackSpellCaster>();
+        if (knockback != null)
+        {
+            knockback.enabled = false;
         }
     }
 }
