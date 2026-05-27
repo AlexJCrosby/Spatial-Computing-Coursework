cad54b910606fa7b09e825920e2e40a8fbb76ef4
Alex <Alex.J.Crosby19@gmail.com>
2026-05-27 15:24:28 +0100

    Tweaking fire trail

--- Assets/Spells Pack/Demo/Scripts/Projectile.cs
+++ Assets/Spells Pack/Demo/Scripts/Projectile.cs
@@ -8,6 +8,7 @@ namespace ZakhanSpellsPack
         public float DestroyExplosion = 4.0f;
         public float DestroyChildren = 2.0f;
         public Vector2 Velocity;
+        private bool hasHit;
 
         Rigidbody rb;
         void Start()
@@ -19,12 +20,27 @@ namespace ZakhanSpellsPack
 
         void OnCollisionEnter(Collision col)
         {
-            var exp = Instantiate(ExplosionPrefab, transform.position, ExplosionPrefab.transform.rotation);
-            Destroy(exp, DestroyExplosion);
-            Transform child;
-            child = transform.GetChild(0);
-            transform.DetachChildren();
-            Destroy(child.gameObject, DestroyChildren);
+            if (hasHit) return;
+            hasHit = true;
+
+            if (ExplosionPrefab != null)
+            {
+                GameObject exp = Instantiate(
+                    ExplosionPrefab,
+                    transform.position,
+                    ExplosionPrefab.transform.rotation
+                );
+
+                Destroy(exp, DestroyExplosion);
+            }
+
+            if (transform.childCount > 0)
+            {
+                Transform child = transform.GetChild(0);
+                child.SetParent(null);
+                Destroy(child.gameObject, DestroyChildren);
+            }
+
             Destroy(gameObject);
         }
     }
